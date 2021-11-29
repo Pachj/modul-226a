ff7511467eabaa4e25cbdfe37fd299bb39bf6655
Henry Joerg <henry.joerg@gmail.com>
2021-11-29 11:20:41 +0100

    05_bot_1 added

---
+++ 05_bot_1/src/main/java/Skateboard.java
@@ -0,0 +1,11 @@
+public class Skateboard {
+    private String form;
+    private int size;
+    private Type type;
+
+    public Skateboard(String form, int size, Type type) {
+        this.form = form;
+        this.size = size;
+        this.type = type;
+    }
+}
